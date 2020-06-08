23302f3bca6dc4e4ed58fac55bdc0c818226983e
OhSeungJin <oiu7934@naver.com>
2020-06-08 18:41:28 +0900

    신정원님 코드 백업

---
+++ code/2020spring_41class_team7-master/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-app>
+    <v-content v-bind:style="{ background: '#8C9EFF'}">
+      <router-view :key="$route.fullPath"></router-view>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+
+  components: {
+
+  },
+
+  data: () => ({
+
+  }),
+};
+</script>
+
+<style>
+
+</style>
