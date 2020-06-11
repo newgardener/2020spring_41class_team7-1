f6fe9f6691e11d6faad69c811d7a445499f2df97
Shin-Hansol <62746223+Shin-Hansol@users.noreply.github.com>
2020-06-11 22:13:24 +0900

    0611일자 백업

---
+++ workspace/신한솔/src/store.js
@@ -0,0 +1,31 @@
+  /* store.js */
+  import Vue from 'vue'
+  import Vuex from 'vuex'
+
+  Vue.use(Vuex)
+
+  export default new Vuex.Store({
+      //state는 변수를 의미함  
+      state: {
+          count: 0,
+          isLogin: false
+      },
+      //mutation 변수를 조작하는 함수
+      mutations: {
+          loginTrue(state) {
+              state.isLogin = true
+          },
+          loginFalse(state) {
+              state.isLogin = false
+          },
+          increment(state) {
+              state.count++
+          },
+          decrement(state) {
+              state.count--
+          }
+      }
+  })
+
+  //사용법 참고
+  //https://kamang-it.tistory.com/entry/Vue14vuex-%EC%82%AC%EC%9A%A9%ED%95%98%EA%B8%B0
