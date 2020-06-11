3e62802f204656820ed952642f22c7111a936070
Jeongwon Shin <30281850+JeongwonShin@users.noreply.github.com>
2020-06-11 23:57:01 +0900

    changed design format

--- code/2020spring_41class_team7-master/src/App.vue
+++ code/2020spring_41class_team7-master/src/App.vue
@@ -1,16 +1,117 @@
 <template>
   <v-app>
-    <v-content v-bind:style="{ background: '#8C9EFF'}">
-      <router-view :key="$route.fullPath"></router-view>
+    <v-content v-bind:style="{ background: '#BFE2FD'}">
+        <v-app-bar
+          color="white"
+          height="100px"
+        >
+        
+        <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
+
+        <router-link class="routerLink" to="/main">
+            <img src="./assets/comparewise.png" height="150" width="300px">
+        </router-link>
+
+    <v-spacer></v-spacer>
+      
+      <router-link 
+        class="routerLink" to="/mypage"
+        v-if="this.$store.state.isLogin"
+      >
+        <v-btn icon class="ma-2">
+          <v-icon x-large>mdi-account-outline</v-icon>
+        </v-btn>
+      </router-link>
+      
+      <v-btn @click="logout()" v-if="this.$store.state.isLogin" class="ma-2" outlined>
+        Logout
+      </v-btn>
+          
+      <router-link 
+        class="routerLink" to="/login"
+        v-if="!this.$store.state.isLogin"
+      >
+        <v-btn class="ma-2" outlined>
+          Login
+        </v-btn>
+      </router-link>
+
+      <router-link 
+        class="routerLink" to="/signup"
+        v-if="!this.$store.state.isLogin"
+      >
+        <v-btn class="ma-2" outlined>
+          Signup
+        </v-btn>
+      </router-link>
+    </v-app-bar>
+
+    <router-view :key="$route.fullPath"></router-view>
+
+<!-- navigation bar  -->
+    <v-navigation-drawer
+      style="height: 25% padding: 10px 10px"
+      v-model="drawer"
+      absolute
+      temporary>
+      <v-list
+        nav
+        dense>
+        <v-list-item-group
+        mandatory color="indigo">
+          <v-list-item>
+            <router-link class="routerLink" to="/main">
+              <v-list-item-icon>
+                  <v-icon>mdi-home</v-icon>            
+              </v-list-item-icon>
+            </router-link>
+            <v-list-item-title>Home</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <router-link class="routerLink" to="/mypage">
+              <v-list-item-icon>
+                  <v-btn icon><v-icon>mdi-account</v-icon></v-btn>
+              </v-list-item-icon>
+            </router-link>
+            <v-list-item-title>My Page</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <router-link class="routerLink" to="/wishlist">
+              <v-list-item-icon>
+                  <v-btn icon><v-icon>mdi-heart</v-icon></v-btn>        
+              </v-list-item-icon>
+            </router-link>
+            <v-list-item-title>Wish List</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <router-link class="routerLink" to="/compare">
+              <v-list-item-icon>
+                  <v-btn icon><v-icon>mdi-scale-balance</v-icon></v-btn>
+              </v-list-item-icon>
+            </router-link>
+            <v-list-item-title>Compare Page</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <router-link class="routerLink" to="/review">
+              <v-list-item-icon>
+                  <v-btn icon><v-icon>mdi-pencil</v-icon></v-btn>
+              </v-list-item-icon>
+            </router-link>
+            <v-list-item-title>Review</v-list-item-title>
+          </v-list-item>
+
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
     </v-content>
   </v-app>
 </template>
 
 <script>
-
-
-
-
 
 export default {
   name: 'App',
@@ -20,7 +121,7 @@ export default {
   },
 
   data: () => ({
-
+    drawer: false
   }),
 };
 </script>
